ec0090500994a5550029d80d86bce0275c44c263
majordomos <alex.micenko@gmail.com>
2019-10-24 19:00:30 +0300

    unit_tests

--- lab3/tpo_Triangle/Triangle/Triangle.cs
+++ lab3/tpo_Triangle/Triangle/Triangle.cs
@@ -4,17 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace tpo_Triangle
+namespace Triangle
 {
-    class Program
+    public class Triangle
     {
-        static void Main(string[] args)
-        {
-        }
-    }
-    public class MyTriangle
-    {
-        public static bool isTriangle(double a, double b, double c)
+        public static bool IsTriangle(double a, double b, double c)
         {
             return (a > 0 && b > 0 && c > 0 && (a + b > c) && (b + c > a) && (c + a > b));
         }
