d377de8af19d358c48cda2f06dd771b2e2c32a9b
majordomos <alex.micenko@gmail.com>
2019-10-31 14:05:03 +0300

    Clean_xUnit

--- lab3/tpo_Triangle/Triangle_Tests/Triangle_Tests.cs
+++ lab3/tpo_Triangle/Triangle_Tests/Triangle_Tests.cs
@@ -5,52 +5,52 @@ namespace Unit_Tests
     public class Triangle_Tests
     {
         [Fact]
-        public void RectangularTriangle()
+        public void IsPossible_RectangularTriangle()
         {
             Assert.True(Triangle.Triangle.IsPossible(3, 4, 5), "Error: non rectangular triangle!");
         }
         [Fact]
-        public void EquilateralTriangle()
+        public void IsPossible_EquilateralTriangle()
         {
             Assert.True(Triangle.Triangle.IsPossible(3, 3, 3), "Error: non equilateral triangle!");
         }
         [Fact]
-        public void UnrealTriangle()
+        public void IsPossible_UnrealTriangle()
         {
             Assert.True(Triangle.Triangle.IsPossible(3, 3, 1), "Error: such triangle can't exists!");
         }
         [Fact]
-        public void WithOneNegativeTriangle()
+        public void IsPossible_WithOneNegativeSide()
         {
             Assert.True(Triangle.Triangle.IsPossible(-7, 8, 9), "Error: triangle has one negative side!");
         }
         [Fact]
-        public void WithTwoNegativeTriangle()
+        public void IsPossible_WithTwoNegativeSides()
         {
             Assert.True(Triangle.Triangle.IsPossible(-6, -7, 8), "Error: triangle has two negative sides!");
         }
         [Fact]
-        public void WithThreeNegativeTriangle()
+        public void IsPossible_WithThreeNegativeSides()
         {
             Assert.True(Triangle.Triangle.IsPossible(-6, -7, -8), "Error: triangle has three negative sides!");
         }
         [Fact]
-        public void WithOneZeroTriangle()
+        public void IsPossible_WithOneZeroSide()
         {
             Assert.True(Triangle.Triangle.IsPossible(0, 6, 7), "Error: triangle has one zero side!");
         }
         [Fact]
-        public void WithTwoZeroTriangle()
+        public void IsPossible_WithTwoZeroSides()
         {
             Assert.True(Triangle.Triangle.IsPossible(0, 0, 7), "Error: triangle has two zero sides!");
         }
         [Fact]
-        public void WithThreeZeroTriangle()
+        public void IsPossible_WithThreeZeroSides()
         {
             Assert.True(Triangle.Triangle.IsPossible(0, 0, 0), "Error: triangle has three zero sides!");
         }
         [Fact]
-        public void WithOneNegativeAndOneZeroTriangle()
+        public void IsPossible_WithNegativeAndZeroSides()
         {
             Assert.True(Triangle.Triangle.IsPossible(-3, 0, 3), "Error: triangle has one negative and one zero side!");
         }
